572b72c529e52eb99794dcc69f781d757d38cfd5
Omkar-Ghongade <omkarsubhashghongade21@gmail.com>
2024-12-01 13:57:18 +0530

    Inital

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const GOOGLE_CLIENT_ID = "344566954706-snr12b4aof3ki0108t6pcfis27d4qjp6.apps.googleusercontent.com"
+
+root.render(
+<React.StrictMode>
+  <GoogleOAuthProvider clientId = {GOOGLE_CLIENT_ID}>
+    <App />
+  </GoogleOAuthProvider>
+</React.StrictMode>
+);
